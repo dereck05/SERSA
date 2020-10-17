aefec2916bedf3a73b1a90aea8f9a8905cdb405b
dereck05 <dereckescalante@hotmail.com>
2020-10-16 22:45:03 -0600

    Init

---
+++ Sersa/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sersa.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
