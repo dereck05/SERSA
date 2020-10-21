3c22b56bde2ed3c487043c12c09b1c8aeb5d9b77
dereck05 <dereckescalante@hotmail.com>
2020-10-21 13:35:03 -0600

    json

--- Sersa/Models/FormularioModel.cs
+++ Sersa/Models/FormularioModel.cs
@@ -2,6 +2,8 @@
 using System.Data;
 using System.Threading.Tasks;
 using MySqlConnector;
+using System.Text.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Sersa
 
@@ -17,11 +19,25 @@ namespace Sersa
         {
             Database = db;
         }
+        public class DateOfBirth
+        {
+            public int year { get; set; }
+            public int month { get; set; }
+            public int day { get; set; }
+            public DateOfBirth(int a, int b, int c)
+            {
+                year = a;
+                month = b;
+                day = c;
+            }
+
+
+        }
 
         public async Task InsertAsync()
         {
             using var cmd = Database.Connection.CreateCommand();
-            cmd.CommandText = @"INSERT INTO `Formulario` (`fecha`, `usuario`, `acueducto`, `encargado`, `telefono`, `funcionario`, `info_general`, `infraestructura`) values (@fecha, @usuario, @acueducto, @encargado, @telefono, @funcionario, @info_general, @infraestructura)";
+            cmd.CommandText = @"INSERT INTO `Formulario` (`fecha`, `usuario`, `acueducto`, `encargado`, `telefono`, `funcionario`, `info_general`, `infraestructura`, `imagen`,`latitud`,`longitud`,`comentarios`) values (@fecha, @usuario, @acueducto, @encargado, @telefono, @funcionario, @info_general, @infraestructura,@imagen,@latitud,@longitud,@comentarios)";
             var timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
 
             cmd.Parameters.AddWithValue("@fecha", timestamp);
@@ -30,9 +46,20 @@ namespace Sersa
             cmd.Parameters.AddWithValue("@encargado", "e");
             cmd.Parameters.AddWithValue("@telefono", "t");
             cmd.Parameters.AddWithValue("@funcionario", "f");
+            DateOfBirth d = new DateOfBirth(1, 2, 3);
+            var json = JsonSerializer.Serialize(d);
+            Console.WriteLine(json);
+            cmd.Parameters.AddWithValue("@info_general",json);
+            cmd.Parameters.AddWithValue("@infraestructura", json);
+            cmd.Parameters.AddWithValue("@imagen", "a");
+            cmd.Parameters.AddWithValue("@latitud", "e");
+            cmd.Parameters.AddWithValue("@longitud", "t");
+            cmd.Parameters.AddWithValue("@comentarios", "f");
             await cmd.ExecuteNonQueryAsync();
 
 
         }
     }
+
+    
 }
