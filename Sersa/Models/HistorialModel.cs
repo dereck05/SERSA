717f3a6087853365ad3661156972f8081e48005f
Gloriana F <glorianafernandezq@hotmail.com>
2020-10-21 16:00:04 -0600

    Agregar modelo historial, modificar controller historial, FormularioModel -> FormulariosModel

---
+++ Sersa/Models/HistorialModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sersa.Models
+{
+    public class HistorialModel
+    {
+        internal DBConnector Database { get; set; }
+
+        public HistorialModel() { }
+
+        internal HistorialModel(DBConnector db)
+        {
+            Database = db;
+        }
+
+        public async Task InsertAsync() 
+        {
+            using var cmd = Database.Connection.CreateCommand();
+            cmd.CommandText = @"INSERT INTO `Historial` (`nombre`, `fecha`, `url`) VALUES (@nombre, @fecha, @url)";
+            var timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+            cmd.Parameters.AddWithValue("@nombre", "nombreEj");
+            cmd.Parameters.AddWithValue("@fecha", timestamp);
+            cmd.Parameters.AddWithValue("@url", "https://www.google.com/");
+            await cmd.ExecuteNonQueryAsync();
+        }
+    }
+}
