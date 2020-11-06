2e034bf7eacfa9612c941d65ba3aa1169b74f6d3
dereck05 <dereckescalante@hotmail.com>
2020-11-05 18:42:37 -0600

    Filtro informe

--- Sersa/Controllers/InformeController.cs
+++ Sersa/Controllers/InformeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Sersa.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +11,28 @@ namespace Sersa.Controllers
 {
     public class InformeController : Controller
     {
-        // GET: /<controller>/
+        internal DBConnector Database { get; set; }
+
+        public InformeController(DBConnector db)
+        {
+            Database = db;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult filtrarInformes()
+        {
+            Database.Connection.OpenAsync();
+            var query = new FormularioInforme(Database);
+            string fIni = "1601510400";
+            string fFin = "1604361600";
+            List<FormularioInforme> lista = query.obtenerFormularioInforme(Int32.Parse(fIni), Int32.Parse(fFin));
+
+            return PartialView("_FormularioInforme",lista);
+
         }
     }
 }
--- Sersa/Models/FormularioInforme.cs
+++ Sersa/Models/FormularioInforme.cs
@@ -1,10 +1,73 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using MySqlConnector;
+
 namespace Sersa.Models
 {
     public class FormularioInforme
     {
-        public FormularioInforme()
+        internal DBConnector Database { get; set; }
+
+        public string id { get; set; }
+        public int fecha { get; set; }
+        public string acueducto { get; set; }
+        public FormularioInforme() { }
+        public FormularioInforme(string idFormulario, string acueductoForm, int fechaForm)
         {
+            id = idFormulario;
+            fecha = fechaForm;
+            acueducto = acueductoForm;
+
+        }
+        public IConfigurationRoot GetConnection()
+
+        {
+
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appSettings.json").Build();
+
+            return builder;
+
+        }
+        internal FormularioInforme(DBConnector db)
+        {
+            Database = db;
+        }
+
+        public List<FormularioInforme> obtenerFormularioInforme(int fechaI, int fechaF)
+        {
+            //Conexion escondida
+            var connection = GetConnection().GetSection("ConnectionStrings").GetSection("Sersa").Value;
+            MySqlConnection conn = new MySqlConnection(connection);
+            conn.Open();
+            string sql = "SELECT id,fecha,acueducto FROM Formulario WHERE fecha between @fechaIni and @fechaFin";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@fechaIni", fechaI);
+            cmd.Parameters.AddWithValue("@fechaFin", fechaF);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            List<FormularioInforme> lista = new List<FormularioInforme>();
+
+            while (rdr.HasRows)
+            {
+
+                while (rdr.Read())
+                {
+                    FormularioInforme temp = new FormularioInforme();
+                    string col1Value = rdr[0].ToString();
+                    temp.id = col1Value;
+                    int col2Value = (int)rdr[1];
+                    temp.fecha = col2Value;
+                    string col3Value = rdr[2].ToString();
+                    temp.acueducto = col3Value;
+                    lista.Add(temp);
+                }
+                rdr.NextResult();
+            }
+
+            return lista;
+
+
         }
     }
 }
