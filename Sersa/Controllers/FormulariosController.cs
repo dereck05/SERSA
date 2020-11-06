530b8781c357daea5651cc6209bec5de8e01e559
Barbara Gutierrez <bgutierrez2499@gmail.com>
2020-11-05 23:29:14 -0600

    Edit FS

--- Sersa/Controllers/FormulariosController.cs
+++ Sersa/Controllers/FormulariosController.cs
@@ -27,7 +27,7 @@ namespace Sersa.Controllers
 
         public async Task<IActionResult> guardarFormularioFS(string FECHA, string ACUEDUCTO, string TOMA, string REGISTRO, string DIRECCION,string ENCARGADO,string TELEFONO,
                     string FUNCIONARIO, string LATITUD,string LONGITUD, string IMG, string LIMPIEZA, string ESPECIFIQUE, string NOTAS, string P1,
-                    string P2, string P3, string P4, string P5, string P6, string P7, string P8, string P9, string P10)
+                    string P2, string P3, string P4, string P5, string P6, string P7, string P8, string P9, string P10, string IOU, string ID)
         {
             FormularioRespuesta f = new FormularioRespuesta();
             f.P1 = P1;
@@ -50,11 +50,21 @@ namespace Sersa.Controllers
 
             await Database.Connection.OpenAsync();
             var query = new FormulariosModel(Database);
-            await query.InsertFormularioFS( FECHA,  ACUEDUCTO,  ENCARGADO,  TELEFONO,
-                     FUNCIONARIO,  LATITUD,  LONGITUD,  IMG, f, ig, NOTAS);
-            var resultado =  new OkObjectResult(query);
+            if (IOU == "I")
+            {
+                await query.InsertFormularioFS(FECHA, ACUEDUCTO, ENCARGADO, TELEFONO,
+                     FUNCIONARIO, LATITUD, LONGITUD, IMG, f, ig, NOTAS);
+                var resultado = new OkObjectResult(query);
+                return resultado;
+            }
+            else
+            {
+                await query.UpdateFormularioFS(FECHA, ACUEDUCTO, ENCARGADO, TELEFONO,
+                     FUNCIONARIO, LATITUD, LONGITUD, IMG, f, ig, NOTAS,ID);
+                var resultado = new OkObjectResult(query);
+                return resultado;
+            }
 
-            return resultado;
         }
 
         public async Task<IActionResult> guardarFormularioFN(string FECHA, string ACUEDUCTO, string NACIENTE, string REGISTRO, string DIRECCION, string ENCARGADO, string TELEFONO,
@@ -416,6 +426,17 @@ namespace Sersa.Controllers
             return PartialView("_LayoutListarPP", lista);
 
         }
+
+        public string obtenerFormularioFS(string ID)
+        {
+
+            Database.Connection.OpenAsync();
+            var query = new FormulariosModel(Database);
+            string resultado = query.obtenerFormularioFS(ID);
+
+            return resultado;
+        }
+
 
     }
 }
