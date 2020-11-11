51f3d20681297fdd26d0469cb75b088c3b2934f2
dereck05 <dereckescalante@hotmail.com>
2020-11-10 18:46:11 -0600

    PDF 1.0

---
+++ Sersa/Models/InformeResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Sersa.Models
+{
+    public class InformeResponse
+    {
+        public string fecha { get; set; }
+        public string acueducto { get; set; }
+        public string encargado { get; set; }
+        public string info_general { get; set; }
+        public string infraestructura { get; set; }
+        public string comentarios { get; set; }
+        public string imagen { get; set; }
+        public string tipo { get; set; }
+        public int si { get; set; }
+        public int no { get; set; }
+        public string riesgo { get; set; }
+
+        public InformeResponse(){}
+    }
+}
